0a9b0d35211d6b18b06dc39567ce908ed2ffaa12
Just Pedro <13648996+juxtapix@users.noreply.github.com>
2018-01-16 21:22:51 -0500

    Update tts_watson_ssml.js

--- examples/01_TTS/node/tts_watson_ssml.js
+++ examples/01_TTS/node/tts_watson_ssml.js
@@ -5,8 +5,8 @@ const util = require('util');
 const fs = require('fs');
 
 function say(something){
-  const user = "34fc6e5d-0c31-4edb-9879-733a85fe8149";
-  const password = "cAo5yxlGdJHm";
+  const user = "USER";
+  const password = "PASS";
   var data = encodeURIComponent(something);
   const voice = "voice=en-US_AllisonVoice";
   command = util.format('curl -X GET -u \"%s:%s\" --output say.mp3 \"https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?accept=audio/mp3&text=\"%s\"&%s\" && afplay say.mp3 ', user,password,data,voice);
