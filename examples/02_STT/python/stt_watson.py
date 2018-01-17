77d2a3ade90c1f1fb1b9ef612760eef236d7aedd
Just Pedro <13648996+juxtapix@users.noreply.github.com>
2018-01-16 21:57:21 -0500

    Update stt_watson.py

--- examples/02_STT/python/stt_watson.py
+++ examples/02_STT/python/stt_watson.py
@@ -5,8 +5,8 @@ import sys
 import pipes
 
 def listen(something):
-    user = "9e0a3437-aa25-4a21-87e0-20fb1d02db63"
-    password = "UPu8SOsrz8eX"
+    user = "USER"
+    password = "PASS"
     data = "@" + pipes.quote(something)
     command = "curl -X POST -u \"{0}:{1}\" -H \"Content-Type: audio/flac\" --data-binary {2} \"https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?timestamps=true&max_alternatives=3\" | grep -m1 \"transcript\" ".format(user,password,data)
     print(command)
