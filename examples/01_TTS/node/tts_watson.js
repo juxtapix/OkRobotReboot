36145100dc61b95f4c5a6ed9c8dcd236452c93a3
Just Pedro <13648996+juxtapix@users.noreply.github.com>
2018-01-16 21:22:27 -0500

    Update tts_watson.js

--- examples/01_TTS/node/tts_watson.js
+++ examples/01_TTS/node/tts_watson.js
@@ -4,8 +4,8 @@ const exec = require('child_process').execSync;
 const util = require('util');
 
 function say(something){
-  const user = "34fc6e5d-0c31-4edb-9879-733a85fe8149";
-  const password = "cAo5yxlGdJHm";
+  const user = "USER";
+  const password = "PASS";
   var data = encodeURIComponent(something);
   const voice = "voice=en-US_AllisonVoice";
   command = util.format('curl -X GET -u \"%s:%s\" --output say.mp3 \"https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?accept=audio/mp3&text=\"%s\"&%s\" && afplay say.mp3 ', user,password,data,voice);
