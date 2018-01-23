c2825965a23569bdf8cd96afd4e4794dd75c5de3
juxtapix <zoodzilla@zoodzillaPro.local>
2018-01-23 16:32:31 -0500

    Updated Comments

--- examples/01_TTS/node/tts_watson.js
+++ examples/01_TTS/node/tts_watson.js
@@ -1,13 +1,15 @@
-//Example TTS - "watson"
+// Example TTS - "watson"
 
-const exec = require('child_process').execSync;
-const util = require('util');
+// require "child_process" to run Child Process Applications
+var exec = require('child_process').execSync;
+// require "util" to access utility functions such as "format"
+var util = require('util');
 
 function say(something){
-  const user = "USER";
-  const password = "PASS";
+  var user = "USER";
+  var password = "PASS";
   var data = encodeURIComponent(something);
-  const voice = "voice=en-US_AllisonVoice";
+  var voice = "voice=en-US_AllisonVoice";
   command = util.format('curl -X GET -u \"%s:%s\" --output say.mp3 \"https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?accept=audio/mp3&text=\"%s\"&%s\" && afplay say.mp3 ', user,password,data,voice);
   exec(command);
 }
--- examples/01_TTS/node/tts_watson_ssml.js
+++ examples/01_TTS/node/tts_watson_ssml.js
@@ -1,14 +1,14 @@
-//Example TTS - "watson with ssml"
+// Example TTS - "watson with ssml"
 
-const exec = require('child_process').execSync;
-const util = require('util');
-const fs = require('fs');
+var exec = require('child_process').execSync;
+var util = require('util');
+var fs = require('fs');
 
 function say(something){
-  const user = "USER";
-  const password = "PASS";
+  var user = "USER";
+  var password = "PASS";
   var data = encodeURIComponent(something);
-  const voice = "voice=en-US_AllisonVoice";
+  var voice = "voice=en-US_AllisonVoice";
   command = util.format('curl -X GET -u \"%s:%s\" --output say.mp3 \"https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?accept=audio/mp3&text=\"%s\"&%s\" && afplay say.mp3 ', user,password,data,voice);
   exec(command);
 }
