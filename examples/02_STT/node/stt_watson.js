258d6ab34f9c5a2f9fd883867b35de7fd6db77d3
Just Pedro <13648996+juxtapix@users.noreply.github.com>
2021-01-19 16:10:20 -0500

    new watson API

--- examples/02_STT/node/stt_watson.js
+++ examples/02_STT/node/stt_watson.js
@@ -4,10 +4,10 @@ var exec = require('child_process').execSync;
 var util = require('util');
 
 function listen(something){
-  var user = "USER";
-  var password = "PASS";
+  var APIkey = "YOUR_API_KEY";
+  var url = "YOUR_API_URL";
   var data = "@" + something;
-  command = util.format('curl -X POST -u \"%s:%s\" -H \"Content-Type: audio/flac\" --data-binary %s \"https://stream.watsonplatform.net/speech-to-text/api/v1/recognize?timestamps=true&max_alternatives=3\" | grep -m1 \"transcript\" ', user,password,data);
+  command = util.format('curl -X POST -u \"apikey:%s\" -H \"Content-Type: audio/flac\" --data-binary %s \"%s/v1/recognize?timestamps=true&max_alternatives=3\" | grep -m1 \"transcript\" ', APIkey,data,url);
   output = exec(command, function(error, stdout, stderr){console.log('stdout: ' + stdout);});
   return output.toString().trim();
 }
