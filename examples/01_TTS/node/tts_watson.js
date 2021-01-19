670e6d51ded894bce68348fd2b4983de119a619c
Just Pedro <13648996+juxtapix@users.noreply.github.com>
2021-01-19 13:36:09 -0500

    New watson API

--- examples/01_TTS/node/tts_watson.js
+++ examples/01_TTS/node/tts_watson.js
@@ -1,17 +1,17 @@
 // Example TTS - "watson"
 
-// require "child_process" to run Child Process Applications
+// required to run Child Process Applications
 var exec = require('child_process').execSync;
-// require "util" to access utility functions such as "format"
+// required to access utility functions such as "format"
 var util = require('util');
 
 function say(something){
-  var user = "USER";
-  var password = "PASS";
+  var APIkey = "YOUR_API_KEY";
+  var url = "YOUR_API_URL";
   var data = encodeURIComponent(something);
   var voice = "voice=en-US_AllisonVoice";
-  command = util.format('curl -X GET -u \"%s:%s\" --output say.mp3 \"https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?accept=audio/mp3&text=\"%s\"&%s\" && afplay say.mp3 ', user,password,data,voice);
+  command = util.format('curl -X GET -u \"apikey:%s\" --output say.mp3 \"%s/v1/synthesize?accept=audio/mp3&text=\"%s\"&%s\" && afplay say.mp3 ', APIkey,url,data,voice);
   exec(command);
 }
 
-say(process.argv[2]);
+say(process.argv[2]); // Request the command line argument after the script name
