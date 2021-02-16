62d7b3712ed845298f6ab4ce1b83086decadbe47
Just Pedro <13648996+juxtapix@users.noreply.github.com>
2021-02-16 11:59:36 -0500

    Update p5_script_basic.js

--- examples/02_STT/_p5/basic/p5_script_basic.js
+++ examples/02_STT/_p5/basic/p5_script_basic.js
@@ -1,9 +1,9 @@
 // p5.SpeechRec - Basic
 var robot = new p5.SpeechRec(); // speech recognition object
-robot.continuous = true; 				// allow continuous recognition
-robot.interimResults = true; 		// allow partial recognition (faster, less accurate)
-robot.onResult = showResult; 		// callback function that triggers when speech is recognized
-robot.onError = showError; 			// callback function that triggers when an error occurs
+robot.continuous = true; 	// allow continuous recognition
+robot.interimResults = true; 	// allow partial recognition (faster, less accurate)
+robot.onResult = showResult; 	// callback function that triggers when speech is recognized
+robot.onError = showError; 	// callback function that triggers when an error occurs
 
 function setup() {
 	listen();
@@ -21,7 +21,7 @@ function listen() {
 
 
 function showResult() {
-  console.log('Transcript: '+ robot.resultString); 			// log the transcript
+  console.log('Transcript: '+ robot.resultString); 		// log the transcript
 	console.log('Confidence: '+ robot.resultConfidence); 	// log the confidence
 }
 
